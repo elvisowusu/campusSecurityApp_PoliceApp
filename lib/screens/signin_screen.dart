7e1d55d76df124bd0e3c33592c508fe47f676d90
elvisowusu <owusuelvisgyasi@gmail.com>
2024-05-28 10:07:01 +0000

    email and password input fields for SignIn Screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,7 +1,7 @@
+import 'package:cs_location_tracker_app/theme/theme.dart';
 import 'package:cs_location_tracker_app/widgets/custom_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -16,30 +16,93 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      image:'assets/images/security.avif',
+      image: 'assets/images/security.avif',
       customContainer: Column(
         children: [
           const Expanded(
-            flex: 1,
-            child: SizedBox(
-              height: 18,) 
-          ),
-          Expanded(
-            flex: 7,
-            child: Container(
-              decoration:const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft:  Radius.circular(40),
-                  topRight: Radius.circular(40),
-                ),
-              ),
-              child: Form(
-                key: _SignInformKey,
-                child:const Column(
+              flex: 1,
+              child: SizedBox(
+                height: 18,
               )),
-            )
-          ),
+          Expanded(
+              flex: 7,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: Form(
+                    key: _SignInformKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w900,
+                            color: lightColorScheme.primary,
+                          ),
+                        ),
+                        TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter Email';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                              label: const Text('Email'),
+                              hintText: 'Enter Email',
+                              hintStyle: const TextStyle(color: Colors.black26),
+                              border: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.black12,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.black12,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              )),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          obscuringCharacter: '*',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter Password';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            label:const Text('Password'),
+                            hintText: 'Enter Password',
+                            hintStyle: const TextStyle(
+                              color: Colors.black26,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide:const BorderSide(
+                                color: Colors.black12
+                              ),
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(
+                                  color: Colors.black12,
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              )
+                          ),
+                        ),
+                      ],
+                    )),
+              )),
         ],
       ),
     );

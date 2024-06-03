a88f2fa62a8ca03ab675b2608b526491a56a0537
elvisowusu <owusuelvisgyasi@gmail.com>
2024-06-03 10:06:28 +0000

    Refactor sign in screen to use CustomScaffold widget and update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:cs_location_tracker_app/firebase_options.dart';
 import 'package:cs_location_tracker_app/screens/welcome_screen.dart';
 import 'package:cs_location_tracker_app/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

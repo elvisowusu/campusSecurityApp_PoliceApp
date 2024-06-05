e98171084026f9536f627b39d13505988041d107
elvisowusu <owusuelvisgyasi@gmail.com>
2024-06-05 16:52:21 +0000

    chore: Update dependencies and add Google sign-in option to the sign-in screen

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -7,6 +7,7 @@ import 'package:cs_location_tracker_app/theme/theme.dart';
 import 'package:cs_location_tracker_app/widgets/custom_scaffold.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class SignInScreen extends StatefulWidget {
@@ -23,6 +24,7 @@ class _SignInScreenState extends State<SignInScreen> {
   bool _isSigningIn = false;
 
   final FirebaseAuthService _auth = FirebaseAuthService();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
@@ -226,7 +228,7 @@ class _SignInScreenState extends State<SignInScreen> {
                           ),
                           SizedBox(
                             child: ElevatedButton(
-                                onPressed: () {},
+                                onPressed: _signInWithGoogle,
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.red,
                                 ),
@@ -318,4 +320,29 @@ class _SignInScreenState extends State<SignInScreen> {
       );
     }
   }
+
+  _signInWithGoogle() async {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  try {
+    final GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+
+      await _firebaseAuth.signInWithCredential(credential);
+      Navigator.push(context, MaterialPageRoute(builder: (e) => const LiveCases()));
+    } else {
+      showToast(message: 'Google sign-in aborted!');
+    }
+  } catch (error) {
+    showToast(message: 'Sign in with Google failed!');
+  }
+}
+
 }

022a9767cd91737825304898175a5f58debfbb32
elvisowusu <owusuelvisgyasi@gmail.com>
2024-06-19 04:07:14 +0000

    Refactor chat textfield to use TextFormField and add message icon enable/disable logic

--- lib/widgets/chat_textfield.dart
+++ lib/widgets/chat_textfield.dart
@@ -9,31 +9,65 @@ class ChatTextField extends StatefulWidget {
 }
 
 class _ChatTextFieldState extends State<ChatTextField> {
+  late TextEditingController messageController;
+  bool isMessageIconEnabled = false;
+  @override
+  void initState() {
+    messageController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Expanded(
-          child: TextField(
+          child: TextFormField(
+            controller: messageController,
+            maxLines: 4,
+            minLines: 1,
+            autofocus: true,
+            onChanged: (value) {
+              value.isEmpty
+                  ? setState(() {
+                      isMessageIconEnabled = false;
+                    })
+                  : setState(() {
+                      isMessageIconEnabled = true;
+                    });
+            },
             decoration: InputDecoration(
-              hintText: 'Type a message',
-              hintStyle: const TextStyle(color: Colors.grey),
-              filled: true,
-              fillColor: Colors.white,
-              isDense: true,
-              border: OutlineInputBorder(  
-                borderSide:const BorderSide(
-                  color: Colors.grey,
-                  width:0,
-                  style: BorderStyle.none
+                hintText: 'Type a message',
+                hintStyle: const TextStyle(color: Colors.grey),
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(
+                      color: Colors.grey, width: 0, style: BorderStyle.none),
+                  borderRadius: BorderRadius.circular(30),
                 ),
-                borderRadius: BorderRadius.circular(30),
-              ),
-            ),
+                suffixIcon: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    RotatedBox(
+                      quarterTurns: 45,
+                      child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.attach_file)),
+                    ),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.camera_alt_outlined))
+                  ],
+                )),
           ),
-        ),
-        const SizedBox(
-          width: 5,
         ),
         const SizedBox(
           width: 5,
@@ -42,7 +76,9 @@ class _ChatTextFieldState extends State<ChatTextField> {
           onPressed: () {
             // Send message to the receiver
           },
-          icon: const Icon(Icons.send),
+          icon: isMessageIconEnabled
+              ? const Icon(Icons.send)
+              : const Icon(Icons.mic_none_outlined),
           color: Colors.blue,
         ),
       ],

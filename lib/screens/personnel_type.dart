3c0da99b3cc201f1b5941a77e7dda03b9771f43a
elvisowusu <owusuelvisgyasi@gmail.com>
2024-07-11 06:05:30 +0000

    blurred the background

--- lib/screens/personnel_type.dart
+++ lib/screens/personnel_type.dart
@@ -1,49 +1,120 @@
 import 'package:cs_location_tracker_app/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:cs_location_tracker_app/widgets/custom_scaffold.dart';
+import 'dart:ui'; // Needed for the BackdropFilter and ImageFilter
 
 class RoleSelectionScreen extends StatelessWidget {
   const RoleSelectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Select Role'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return CustomScaffold(
+      image: 'assets/images/security.avif',
+      customContainer: Column(
         children: [
-          const Text('Choose your Role'),
-          GestureDetector(
-            onTap: () {
-              // Navigate to sign-up page with the selected role
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SignUpScreen(role: 'Police Officer'),
-                ),
-              );
-            },
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              color: Colors.blue,
-              child: const Text('Police Officer'),
+          const Expanded(
+            flex: 7,
+            child: SizedBox(
+              height: 18,
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              // Navigate to sign-up page with the selected role
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const SignUpScreen(role: 'Counsellor'),
+          Expanded(
+            flex: 7,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 95, 92, 92).withOpacity(0.2),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(25, 105, 25, 10),
+                        child: Column(
+                          children: [
+                            const Text(
+                              'Choose your Role',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.white,
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen(role: 'Police Officer'),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                width: double.infinity,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  color: Colors.white12,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Police Officer",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) =>
+                                        const SignUpScreen(role: 'Counsellor'),
+                                  ),
+                                );
+                              },
+                              child: Container(
+                                width: double.infinity,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  color: Colors.white12,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                  child: Text(
+                                    "Counsellor",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              );
-            },
-            child: Container(
-              padding: const EdgeInsets.all(10),
-              color: Colors.blue,
-              child: const Text('Counsellor'),
+              ],
             ),
           ),
         ],
